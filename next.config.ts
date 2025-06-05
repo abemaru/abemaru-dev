b6bba51236911e9affcb87846998eb7438cc72b7
abemaru <awebo.abemaru@gmail.com>
2025-06-06 08:37:42 +0900

    img追加

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["pbs.twimg.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        port: "",
+        pathname: "/profile_images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,31 @@
+import Image from "next/image";
+
 export default function AboutPage() {
   return (
-    <div className="p-8 pb-20 gap-16 sm:p-20">
+    <main className="p-8 pb-20 gap-16 sm:p-20">
       <div className="max-w-4xl mx-auto">
         <h1 className="text-4xl font-bold mb-8">わたしについて</h1>
+        <div className="flex justify-center ">
+          <div className="relative
+            w-32 h-32
+            sm:w-48 sm:h-48
+            md:w-56 md:h-56
+            lg:w-64 lg:h-64
+            xl:w-72 xl:y-72
+            ">
+            <Image
+              src="https://pbs.twimg.com/profile_images/1544705950418075648/hD2Cod2s_400x400.jpg"
+              alt="profileImg"
+              fill
+              className="rounded-full object-cover border-4 border-blue-800 shadow-xl"
+              sizes="(max-width: 640px) 80px, 180px"
+              priority
+            />
+          </div>
+        </div>
+        <div>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
