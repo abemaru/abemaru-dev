800e5381759fada9607a1fdce2df827aaa0274ae
abemaru <awebo.abemaru@gmail.com>
2025-06-05 11:22:41 +0900

    私についてページの追加

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div className="p-8 pb-20 gap-16 sm:p-20">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold mb-8">わたしについて</h1>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
         <ThemeProvider>
           <Header />
